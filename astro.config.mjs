0561543ae9135069745d9698460e3908b26ac531
jbhuckabee <jbhuckabee@gmail.com>
2025-06-16 13:43:11 -0500

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://JoshuaBHuckabee.github.io',
-    base: '/astro-tut/'
+    base: '/astro-tut',
 });
